5ea7d0f4ff7551d965a94ff58a5ad0b99016d52d
RyanFatsena <71627584+RyanFatsena@users.noreply.github.com>
2020-10-14 19:43:14 +0700

    Create lthn5.py

---
+++ lthn5.py
@@ -0,0 +1,11 @@
+# Program untuk membuat 'diagram' jumlah siswa
+
+print('============== Program Membuat Diagram Jumlah Siswa =============='+'\n')
+
+lakiLaki = int(input('Berapa jumlah mahasiswa laki-laki di jurusan PTIK UNS ?'))
+perempuan = int(input('Berapa jumlah mahasiswa perempuan di jurusan PTIK UNS ?'))
+
+print('=============='+'\n')
+
+print('Laki-laki = '+'*' * lakiLaki)
+print('Perempuan = '+'*' * perempuan)
